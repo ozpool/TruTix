5e313e2dac2bc76350709e1ff66a037eee1c17df
ozpool <jittendersingh389@gmail.com>
2026-06-11 14:31:13 +0530

    feat(server): chain event indexer syncing owner and listing caches

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import { config } from "./config";
 import { connectDb } from "./db";
 import { createApp } from "./app";
+import { startIndexer } from "./indexer/start";
 
 async function main() {
   await connectDb();
+  startIndexer();
   const app = createApp();
   app.listen(config.PORT, () => {
     console.log(`API listening on :${config.PORT}`);
